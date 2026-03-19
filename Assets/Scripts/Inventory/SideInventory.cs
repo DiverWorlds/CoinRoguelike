bce4dca7c1b588f7694c6f1022357887ba2192f9
uwatcha <alterego102u3v3@gmail.com>
2026-03-20 04:11:30 +0900

    各面ずつのside入手機能追加

--- Assets/Scripts/Inventory/SideInventory.cs
+++ Assets/Scripts/Inventory/SideInventory.cs
@@ -16,22 +16,26 @@ public class SideInventory : MonoBehaviour
         sides.Remove(side);
     }
 
+    public List<BaseSide> GetSortedByStrength(FrontAndBack frontOrBack, bool isAscending)
+    {
+        IEnumerable<BaseSide> targetSides = sides.Where(side => side.FrontOrBack == frontOrBack);
+
+        return isAscending
+            ? targetSides.OrderBy(side => side.Strength).ToList()
+            : targetSides.OrderByDescending(side => side.Strength).ToList();
+    }
+
+    public List<BaseSide> GetSortedByWeight(FrontAndBack frontOrBack, bool isAscending)
+    {
+        IEnumerable<BaseSide> targetSides = sides.Where(side => side.FrontOrBack == frontOrBack);
+
+        return isAscending
+            ? targetSides.OrderBy(side => side.Weight).ToList()
+            : targetSides.OrderByDescending(side => side.Weight).ToList();
+    }
     public List<BaseSide> GetByFrontOrBack(FrontAndBack frontOrBack)
     {
         return sides.Where(side => side.FrontOrBack == frontOrBack).ToList();
     }
 
-    public List<BaseSide> GetSortedByFrontBackAndStrength(bool isStrengthAscending)
-    {
-        return isStrengthAscending
-            ? sides.OrderBy(side => side.FrontOrBack).ThenBy(side => side.Strength).ToList()
-            : sides.OrderBy(side => side.FrontOrBack).ThenByDescending(side => side.Strength).ToList();
-    }
-
-    public List<BaseSide> GetSortedByFrontBackAndWeight(bool isWeightAscending)
-    {
-        return isWeightAscending
-            ? sides.OrderBy(side => side.FrontOrBack).ThenBy(side => side.Weight).ToList()
-            : sides.OrderBy(side => side.FrontOrBack).ThenByDescending(side => side.Weight).ToList();
-    }
 }
