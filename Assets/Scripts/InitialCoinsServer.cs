93be9a32f1685bca71c7d5a1983585673d80330e
uwatcha <alterego102u3v3@gmail.com>
2026-03-19 20:31:51 +0900

    Side関連のバグ取れた

--- Assets/Scripts/InitialCoinsServer.cs
+++ Assets/Scripts/InitialCoinsServer.cs
@@ -15,12 +15,32 @@ public class InitialCoinsServer : MonoBehaviour
         foreach (var side in sidesServer.GetSides())
         {
             BaseSide newSide = Instantiate(side.gameObject, sideInventory.transform).GetComponent<BaseSide>();
+            newSide.Initialize();
             sides.Add(newSide);
         }
+
+        //TODO: デバッグ用に足りないSideを雑に生成してるので、ちゃんとした方法で初期コインを生成するようにする
+        for (int i = 0; i < 2; i++)
+        {
+            foreach (var side in sidesServer.GetSides())
+            {
+                BaseSide newSide = Instantiate(side.gameObject, sideInventory.transform).GetComponent<BaseSide>();
+                newSide.Initialize();
+                sides.Add(newSide);
+            }
+        }
+
+        
+
+                foreach (var side in sideInventory.GetComponentsInChildren<BaseSide>(true))
+                {
+                    Debug.Log($"sideInventory child BaseSide: {side.EffectName}");
+                }
+
         //TODO: 雑に作ってるので直す
         for (int i = 0; i < 3; i++)
         {
-            Coin newCoin = coinFactory.CreateCoin(sides[0], sides[1]);
+            Coin newCoin = coinFactory.CreateCoin(sides[2*i], sides[2*i+1]);
             coinInventory.Add(newCoin);
         }
         Debug.Log("InitialCoinsServer.Start() finished");
--- Assets/Scripts/SidesServer.cs
+++ Assets/Scripts/SidesServer.cs
@@ -16,9 +16,6 @@ public class SidesServer : MonoBehaviour
     }
     public List<BaseSide> GetSides()
     {
-        Debug.Log($"source: {sides[0].EffectName}, {sides[1].EffectName}");
-        List<BaseSide> sidesCopy = new List<BaseSide>(sides);
-        Debug.Log($"copied: {sidesCopy[0].EffectName}, {sidesCopy[1].EffectName}");
-        return sidesCopy;
+        return sides;
     }
 }
