dfb245eda0f07fa568c3b2972fecc64dba8c8a76
uwatcha <alterego102u3v3@gmail.com>
2026-03-22 15:13:47 +0900

    tmp

--- Assets/Scripts/Inventory/CoinInventory.cs
+++ Assets/Scripts/Inventory/CoinInventory.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class CoinInventory : MonoBehaviour
 {
+    [SerializeField] private CoinSlotAssorter coinSlotAssorter;
     private Action<Coin> onCoinAdded;
     private Action<Coin> onCoinRemoved;
 
     //TODO: コインの種類で自動ソート機能を追加するかも
+    public CoinSlotAssorter CoinSlotAssorter => coinSlotAssorter;
     public const int MinCoinCount = 1;
     public const int MaxCoinCount = 3;
 
@@ -51,6 +53,22 @@ public class CoinInventory : MonoBehaviour
         Logger.Log("onCoinRemoved invoked for coin: " + (coin != null ? $"{coin.FrontSideValue} / {coin.BackSideValue}" : "null") + ", isRemoved: " + isRemoved);
 
         return isRemoved;
+    }
+    public bool RemoveRandom()
+    {
+        if (coins.Count <= MinCoinCount)
+        {
+            return false;
+        }
+
+        int randomIndex = UnityEngine.Random.Range(0, coins.Count);
+        Coin coinToRemove = coins[randomIndex];
+        coins.RemoveAt(randomIndex);
+        Destroy(coinToRemove.gameObject);
+        onCoinRemoved?.Invoke(coinToRemove);
+        Logger.Log("onCoinRemoved invoked for coin: " + (coinToRemove != null ? $"{coinToRemove.FrontSideValue} / {coinToRemove.BackSideValue}" : "null") + ", isRemoved: true");
+
+        return true;
     }
 
     public Coin GetCoin(int index)
